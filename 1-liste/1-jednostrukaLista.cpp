91d8b76b842bb50533b05adc2d975b0c0b077ebd
mina <minaduranovic@gmail.com>
2023-11-11 21:09:40 +0100

    fali listaStek

--- 1-liste/1-jednostrukaLista.cpp
+++ 1-liste/1-jednostrukaLista.cpp
@@ -3,6 +3,23 @@
 
 using namespace std;
 
+template <typename tip> class Lista {
+public:
+  Lista() {}
+  virtual ~Lista() {}
+  virtual int brojElemenata() const = 0;
+  virtual tip &trenutni() = 0;
+  virtual tip trenutni() const = 0;
+  virtual bool sljedeci() = 0;
+  virtual bool prethodni() = 0;
+  virtual void pocetak() = 0;
+  virtual void kraj() = 0;
+  virtual void obrisi() = 0;
+  virtual void dodajIspred(const tip &el) = 0;
+  virtual void dodajIza(const tip &el) = 0;
+  virtual tip &operator[](int i) = 0;
+  virtual tip operator[](int i) const = 0;
+};
 template <typename tip> class JednostrukaLista : public Lista<tip> {
 private:
   struct Cvor {
